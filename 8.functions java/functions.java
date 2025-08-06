334de8514d65c90a8f92b54d522fcd837ae027a8
PAVAN-ENG-SIT <valupadasu.pavanteja@gmail.com>
2025-08-06 17:48:37 +0530

    functions java

---
+++ 8.functions java/functions.java
@@ -0,0 +1,18 @@
+public class functions {
+    public static void main(String[] args){
+        int sum=0;
+        int a=100;
+        int b=20;
+        sum=add(a,b);
+        
+        System.out.println(sum);
+    }   
+    
+    static int add(int x,int y){
+        x=50;
+        return x + y;
+    
+    }
+
+
+}
---
+++ 8.functions java/paraArgs.java
@@ -0,0 +1,12 @@
+public class paraArgs{
+    public static void main(String[] args) {
+        sum(8,-5);
+    }
+
+    public static int sum(int p,int q){
+        return p+q;
+
+    }
+}
+
+
